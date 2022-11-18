52b1f5d910ece0d6c9fdb36c9a908b08b83798c6
Bruna Ferreira <51390917+bugelseif@users.noreply.github.com>
2022-11-18 10:23:32 -0300

    Atualização de cards

--- app/process/tempo.py
+++ app/process/tempo.py
@@ -1,14 +1,40 @@
 import random
-
+from datetime import date
 def previsao(cidade):
     cidades = open("app\process\cidades.txt", 'r').readlines()
+    data= date.today()
     if (cidade+'\n') in cidades:
-        resultado = {
+        resultado = [{
             'cidade': cidade,
+            'data':((data.day),(data.month)),
             'clima': random.choice(climas),
             'temperatura' : random.randrange(1,40),
             'umidade': random.randrange(1,100)
-        }
+        },{
+            'cidade': cidade,
+            'data':((data.day+1),(data.month)),
+            'clima': random.choice(climas),
+            'temperatura' : random.randrange(1,40),
+            'umidade': random.randrange(1,100)
+        },{
+            'cidade': cidade,
+            'data':((data.day+2),(data.month)),
+            'clima': random.choice(climas),
+            'temperatura' : random.randrange(1,40),
+            'umidade': random.randrange(1,100)
+        },{
+            'cidade': cidade,
+            'data':((data.day+3),(data.month)),
+            'clima': random.choice(climas),
+            'temperatura' : random.randrange(1,40),
+            'umidade': random.randrange(1,100)
+        },{
+            'cidade': cidade,
+            'data':((data.day+4),(data.month)),
+            'clima': random.choice(climas),
+            'temperatura' : random.randrange(1,40),
+            'umidade': random.randrange(1,100)
+        }]
         return resultado
     return None
 
